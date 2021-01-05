70425487a87d14a31464aeba2b5f5000b464e653
Jack Draak <JackDraak@gmail.com>
2021-01-05 18:03:49 -0500

    Re-tooled to add is_solved() boolean property to Game (formerly Grid) class.
    Avoiding n/2 unsolvable puzzles by not randomizing the initial game grid; will need to add a shuffle function.

--- main.py
+++ main.py
@@ -3,22 +3,22 @@
 # (C) 2021 Jack Draak
 
 
-import random  # Not strictly required; used to randomize the starting-grid
+import random  # will be used soon for shuffling the board and/or seeking a solution
 
 
-class Grid:
+class Game:
     def __init__(self, size):
-        self.size = size
-        labels = []
-        for label in range(1, size * size):
-            labels.append(label)
-        self.tiles = []
+        tiles = []
+        this_label = 0
         for row in range(size):
             for column in range(size):
-                if len(labels) > 0:
-                    random_label = labels.pop(random.randint(0, len(labels) - 1))
-                    self.tiles.append(Tile(random_label, row, column))
+                this_label += 1
+                tiles.append(Tile(this_label, row, column))
+        self.free_label = str(size * size)
         self.free_position = size - 1, size - 1
+        self.size = size
+        self.tiles = tiles
+        self.solution = tuple(self.get_tile_set())
 
     def __repr__(self):
         print_string = str()
@@ -26,17 +26,25 @@ class Grid:
             print_string += "\t"
             for y in range(self.size):
                 label = self.get_tile_label(x, y)
-                if label is None:
+                if label is None or label and int(label) is self.size * self.size:
                     print_string += "\t"
                 else:
                     print_string += f"\t{self.get_tile_label(x, y)}"
             print_string += "\n"
+        # print_string += str(f"Solved: {self.is_solved()}")  # debug
         return print_string
 
     def get_tile_label(self, row, column):
         for tile in self.tiles:
             if tile.row == row and tile.column == column:
                 return tile.label
+
+    def get_tile_set(self):
+        tile_set = []
+        for x in range(self.size):
+            for y in range(self.size):
+                tile_set.append(self.get_tile_label(x, y))
+        return tile_set
 
     def get_tile_position(self, label):
         for tile in self.tiles:
@@ -54,24 +62,29 @@ class Grid:
             if tile.column == free_column:
                 if tile.row + 1 == free_row or tile.row - 1 == free_row:
                     valid_moves.append(tile.label)
+        if valid_moves.__contains__(self.free_label):
+            valid_moves.remove(self.free_label)
         return valid_moves
 
-    def move_tile(self, value):
-        valid_moves = self.get_valid_moves()
-        if valid_moves.__contains__(value):
-            old_free_position = self.free_position
-            self.free_position = self.get_tile_position(value)
-            if self.set_tile_position(value, old_free_position[0], old_free_position[1]):
-                return True
-            else:
-                self.free_position = old_free_position
-        return False
+    def is_solved(self):
+        return tuple(self.get_tile_set()) == self.solution
 
     def set_tile_position(self, label, row, column):
         for tile in self.tiles:
             if tile.label == label:
-                tile.set_position(row, column)
+                tile.move_to(row, column)
                 return True
+        return False
+
+    def slide_tile(self, label):
+        valid_moves = self.get_valid_moves()
+        if valid_moves.__contains__(label):
+            swap_free_pos = self.free_position
+            swap_tile_pos = self.get_tile_position(label)
+            self.set_tile_position(label, swap_free_pos[0], swap_free_pos[1])
+            self.set_tile_position(self.free_label, swap_tile_pos[0], swap_tile_pos[1])
+            self.free_position = swap_tile_pos[0], swap_tile_pos[1]
+            return True
         return False
 
 
@@ -81,37 +94,45 @@ class Tile:
         self.row = row
         self.column = column
 
-    # only used for debugging purposes to view a tile's meta-data
     def __repr__(self):
-        print(f"Tile: {self.label}, position: {self.row}, {self.column}")
+        print_string = str(f"<Tile> label:{self.label}, position: {self.row}, {self.column}")
+        return print_string
 
-    def set_position(self, row, column):
+    def move_to(self, row, column):
         self.row = row
         self.column = column
 
 
-def input_grid_size():
-    size = 0
+def input_game_size():
+    size = int()
     print("To play the classic tile game, '15', ", end="")
     no_intention = True
     while no_intention:
         intention = input("please enter a grid size greater than 2, and less than 12 [default: 4] ")
         if intention == "":
-            size = 4
+            size = 4  # default
             no_intention = False
         elif intention.isdigit():
             size = int(intention)
             if 2 < size < 12:
                 no_intention = False
+    print()
     return size
 
 
-if __name__ == '__main__':
-    # Initialize and display a new Grid-class game object of the specified size:
-    game = Grid(input_grid_size())
-    print(game)
-
-    # Move the tiles, one-by-one, until you get bored:
+def play(game):
+    # Move the tiles, one-by-one, until player get bored:
     while True:
-        while game.move_tile(input("enter the label of the tile you would like to move: ")):
-            print(game)
+        print(game)
+        # print(game.get_tile_set())  # debug
+        # print(game.get_valid_moves())  # debug
+        input_string = \
+            str(f"Please, enter the label of the tile you would like to move\nvalid plays: {game.get_valid_moves()} ")
+        player_move = input(input_string)
+        print()
+        if not game.slide_tile(player_move):
+            print("Input not understood...\n")
+
+
+if __name__ == '__main__':
+    play(Game(input_game_size()))

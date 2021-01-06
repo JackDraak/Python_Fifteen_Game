489c1f3e8473edec260158807be97b5c4eeb66b4
Jack Draak <JackDraak@gmail.com>
2021-01-05 21:35:10 -0500

    more tidy

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 # Playing around with Python 3, continued:
-# the game "Fifteen", for the console:
+# the classic game "Fifteen", for the console:
 # (C) 2021 Jack Draak
 
 
-import random  # will be used soon for shuffling the board and/or seeking a solution
+import random
 
 
 class Game:
     def __init__(self, size):
-        self.free_label = str(size * size)
-        self.free_position = size - 1, size - 1
+        self.blank_label = str(size * size)
+        self.blank_position = size - 1, size - 1
+        self.shuffle_default = size * 25
         self.size = size
         tiles = []
         this_label = 0
@@ -19,7 +20,7 @@ class Game:
                 tiles.append(Tile(this_label, row, column))
         self.tiles = tiles
         self.solution = self.get_tile_set()
-        self.shuffle(100)  # starting with a "light" shuffle
+        self.shuffle(self.shuffle_default)
 
     def __repr__(self):
         print_string = str()
@@ -27,18 +28,23 @@ class Game:
             print_string += "\t"
             for y in range(self.size):
                 label = self.get_tile_label(x, y)
-                if label is None or label and int(label) is self.size * self.size:
-                    print_string += "\t"
+                if label != self.blank_label:
+                    print_string += f"\t{label}"
                 else:
-                    print_string += f"\t{self.get_tile_label(x, y)}"
+                    print_string += "\t"
             print_string += "\n"
-        # print_string += str(f"Solved: {self.is_solved()}")  # debug
         return print_string
 
     def get_tile_label(self, row, column):
         for tile in self.tiles:
             if tile.row == row and tile.column == column:
                 return tile.label
+
+    def get_tile_position(self, label):
+        for tile in self.tiles:
+            if tile.label == label:
+                return tile.row, tile.column
+        return False
 
     def get_tile_set(self):
         tile_set = []
@@ -47,15 +53,9 @@ class Game:
                 tile_set.append(self.get_tile_label(x, y))
         return tile_set
 
-    def get_tile_position(self, label):
-        for tile in self.tiles:
-            if tile.label == label:
-                return tile.row, tile.column
-        return False
-
     def get_valid_moves(self):
         valid_moves = []
-        free_row, free_column = self.free_position
+        free_row, free_column = self.blank_position
         for tile in self.tiles:
             if tile.row == free_row:
                 if tile.column + 1 == free_column or tile.column - 1 == free_column:
@@ -63,12 +63,12 @@ class Game:
             if tile.column == free_column:
                 if tile.row + 1 == free_row or tile.row - 1 == free_row:
                     valid_moves.append(tile.label)
-        if valid_moves.__contains__(self.free_label):
-            valid_moves.remove(self.free_label)
+        if valid_moves.__contains__(self.blank_label):
+            valid_moves.remove(self.blank_label)
         return valid_moves
 
     def is_solved(self):
-        return self.get_tile_set() == self.solution
+        return self.solution == self.get_tile_set()
 
     def set_tile_position(self, label, row, column):
         for tile in self.tiles:
@@ -85,13 +85,12 @@ class Game:
             cycles -= 1
 
     def slide_tile(self, label):
-        valid_moves = self.get_valid_moves()
-        if valid_moves.__contains__(label):
-            swap_free_pos = self.free_position
+        if self.get_valid_moves().__contains__(label):
+            swap_free_pos = self.blank_position
             swap_tile_pos = self.get_tile_position(label)
             self.set_tile_position(label, swap_free_pos[0], swap_free_pos[1])
-            self.set_tile_position(self.free_label, swap_tile_pos[0], swap_tile_pos[1])
-            self.free_position = swap_tile_pos[0], swap_tile_pos[1]
+            self.set_tile_position(self.blank_label, swap_tile_pos[0], swap_tile_pos[1])
+            self.blank_position = swap_tile_pos[0], swap_tile_pos[1]
             return True
         return False
 
@@ -103,8 +102,7 @@ class Tile:
         self.column = column
 
     def __repr__(self):
-        print_string = str(f"<Tile> label:{self.label}, position: {self.row}, {self.column}")
-        return print_string
+        return str(f"<Tile> label:{self.label}, position: {self.row}, {self.column}")
 
     def move_to(self, row, column):
         self.row = row
@@ -112,44 +110,54 @@ class Tile:
 
 
 def input_game_size():
-    size = int()
+    size_default = 4
+    size_max = 12
+    size_min = 3
     print("\nTo play the classic tile game, '15', ", end="")
     no_intention = True
     while no_intention:
-        intention = input("please enter a grid size greater than 2, and less than 12 [default: 4] " +
+        intention = input(f"please chose a grid size from {size_min} to {size_max} [default: {size_default}] " +
                           "\n(the goal of the game is to slide the game tiles into the 'open' position, 1-by-1, " +
                           "until the tiles are in-order.) ")
         if intention == "":
-            size = 4  # default
+            size = size_default
             no_intention = False
         elif intention.isdigit():
             size = int(intention)
-            if 2 < size < 12:
+            if size_min <= size <= size_max:
                 no_intention = False
     print()
     return size
 
 
+def input_shuffle(game):
+    print("Congratulations, you solved the puzzle! ")
+    print(game)
+    shuffles = ""
+    shuffles_default = str(game.shuffle_default)
+    while not shuffles.isdigit():
+        shuffles = input(f"How many times would you like to shuffle? [default: {shuffles_default}] ")
+        if shuffles == "":
+            shuffles = shuffles_default
+    game.shuffle(int(shuffles))
+
+
+def input_turn(game):
+    print(game)
+    input_string = \
+        str("Please, enter the label of the tile you would like to push into the gap.\n" +
+            f"valid plays: {game.get_valid_moves()} ")
+    player_move = input(input_string)
+    print()
+    if not game.slide_tile(player_move):
+        print("Input not understood...\n")
+
+
 def play(game):
-    # Move the tiles, one-by-one, until player get bored:
     while True:
-        print(game)
-        # print(game.get_tile_set())  # debug
-        # print(game.get_valid_moves())  # debug
-        input_string = \
-            str("Please, enter the label of the tile you would like to push into the gap\n" +
-                f"valid plays: {game.get_valid_moves()} ")
-        player_move = input(input_string)
-        print()
-        if not game.slide_tile(player_move):
-            print("Input not understood...\n")
+        input_turn(game)
         if game.is_solved():
-            print("Congratulations, you solved the puzzle!")
-            print(game)
-            shuffles = ""
-            while not shuffles.isdigit():
-                shuffles = input("How many times would you like to shuffle? [>100 advised] ")
-            game.shuffle(int(shuffles))
+            input_shuffle(game)
 
 
 if __name__ == '__main__':
